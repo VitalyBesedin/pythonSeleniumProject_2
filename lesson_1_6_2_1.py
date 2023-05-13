2661ee1feeee7a7ff2780f51c7991d0d721c2a09
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-13 20:14:42 +0300

    Initiation selenium project

---
+++ lesson_1_6_2_1.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service()
+browser = webdriver.Chrome(options=options, service=g)
+#browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/simple_form_find_task.html")
+button = browser.find_element(By.ID, "submit_button")
+
+
+#time.sleep(5)
+
+# закрываем браузер после всех манипуляций
+# browser.quit()
+browser.close()
