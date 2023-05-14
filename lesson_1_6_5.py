0a4d2485a589f41b19537ff8faeeb36b4e40aea5
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-14 17:54:43 +0300

    it works

--- lesson_1_6_5.py
+++ lesson_1_6_5.py
@@ -1,41 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import math
 
-url = "https://www.degreesymbol.net/"
+base_url = "http://suninjuly.github.io/find_link_text"
 
-# try:
-# browser = webdriver.Chrome()
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-g = Service()
-browser = webdriver.Chrome(options=options, service=g)
-# browser = webdriver.Safari()
-browser.get(url)
-browser.maximize_window()
+try:
+    browser = webdriver.Chrome()
+    browser.get(base_url)
+    link = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link.click()
 
-    # input1 = browser.find_element(By.TAG_NAME, 'div:first-child > input')
-# link = browser.find_element(By.LINK_TEXT, "Degree Symbol in Math")
-# link.click()
-link = browser.find_element(By.PARTIAL_LINK_TEXT, 'Math')
-link.click()
-time.sleep(10)
-    # input1 = browser.find_element(By.TAG_NAME, 'input')
-    # input1.send_keys("Ivan")
-    # input2 = browser.find_element(By.NAME, 'last_name')
-    # input2.send_keys("Petrov")
-    # input3 = browser.find_element(By.CLASS_NAME, 'city')
-    # input3.send_keys("Smolensk")
-    # input4 = browser.find_element(By.ID, "country")
-    # input4.send_keys("Russia")
-    # button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    # button.click()
 
-# finally:
-#     # успеваем скопировать код за 30 секунд
-#     time.sleep(30)
-#     # закрываем браузер после всех манипуляций
-#     browser.quit()
+    input1 = browser.find_element(By.TAG_NAME, 'input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, 'last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, 'city')
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
 
-# не забываем оставить пустую строку в конце файла ansver: 22.343046454729887
+finally:
+    # успеваем скопировать код за 5 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
