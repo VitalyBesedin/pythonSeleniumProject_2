b37293430b517d5b24a85d313b2b39d3384e3179
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-14 12:46:45 +0300

    truing to click link

--- lesson_1_6_5.py
+++ lesson_1_6_5.py
@@ -1,29 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+url = "https://www.degreesymbol.net/"
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
+# try:
+# browser = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service()
+browser = webdriver.Chrome(options=options, service=g)
+# browser = webdriver.Safari()
+browser.get(url)
+browser.maximize_window()
 
     # input1 = browser.find_element(By.TAG_NAME, 'div:first-child > input')
-    input1 = browser.find_element(By.TAG_NAME, 'input')
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, 'last_name')
-    input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, 'city')
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+# link = browser.find_element(By.LINK_TEXT, "Degree Symbol in Math")
+# link.click()
+link = browser.find_element(By.PARTIAL_LINK_TEXT, 'Math')
+link.click()
+time.sleep(10)
+    # input1 = browser.find_element(By.TAG_NAME, 'input')
+    # input1.send_keys("Ivan")
+    # input2 = browser.find_element(By.NAME, 'last_name')
+    # input2.send_keys("Petrov")
+    # input3 = browser.find_element(By.CLASS_NAME, 'city')
+    # input3.send_keys("Smolensk")
+    # input4 = browser.find_element(By.ID, "country")
+    # input4.send_keys("Russia")
+    # button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    # button.click()
 
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+# finally:
+#     # успеваем скопировать код за 30 секунд
+#     time.sleep(30)
+#     # закрываем браузер после всех манипуляций
+#     browser.quit()
 
 # не забываем оставить пустую строку в конце файла ansver: 22.343046454729887
